de57d994a6e42b418988f3dcf6b514d6976690ac
Nikhil Kumar <nikhilgraphicd@gmail.com>
2024-07-25 14:58:47 +0530

    ta

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, send, emit
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "your_secret_key"
+CORS(app, resources={r"/*": {"origins": "*"}})
+socketio = SocketIO(app, cors_allowed_origins="*")
+
 
 @app.route("/")
-def start():
-    return "The MBSA Server is Running"
+def index():
+    return {"message": "Jobs Scrapper!"}
 
-@app.route("/mbsa")
-def mbsa():
-    return render_template('index.html')
+
+@socketio.on("message")
+def message(message):
+    print("Received message: " + str(message))
+    emit("response", message)
+
+
+@socketio.on("scrapeInternhsala")
+def scrapeInternhsala(msg):
+    try:
+        emit("response", {"GOT IT MAN! GOT IT!: ", msg})
+    except KeyError as e:
+        print(f"KeyError: {e} - The session may be disconnected.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    socketio.run(app, allow_unsafe_werkzeug=True)
